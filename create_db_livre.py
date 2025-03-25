05778755bb6d60ed17f311d97398c5c5eb564608
Makrix <140716576+Makrix78@users.noreply.github.com>
2025-03-25 10:16:04 +0100

    Update create_db_livre.py

--- create_db_livre.py
+++ create_db_livre.py
@@ -1,5 +1,5 @@
 import sqlite3
-#dd
+
 connection = sqlite3.connect('database.db')
 
 with open('schema.sql') as f:
@@ -7,8 +7,15 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(1, 'Emilie', 'Victor', 2024, 10))
-cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(2, 'Didier', 'Laurent', 2023, 5))
+# Ajouter des livres
+cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)", (1, 'Emilie', 'Victor', 2024, 10))
+cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)", (2, 'Didier', 'Laurent', 2023, 5))
+
+# Ajouter un utilisateur admin
+cur.execute("""
+INSERT INTO Utilisateurs (ID_utilisateur, Nom, Prenom, Email, Mot_de_passe, Role)
+VALUES (?, ?, ?, ?, ?, ?)
+""", (1, 'Admin', 'Super', 'admin@biblio.com', 'password', 'admin'))
 
 connection.commit()
 connection.close()
