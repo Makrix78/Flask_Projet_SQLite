484986208644cc1bd05aa96998db89a5e39e1713
Makrix <140716576+Makrix78@users.noreply.github.com>
2025-03-25 09:33:24 +0100

    Update create_db_livre.py

--- create_db_livre.py
+++ create_db_livre.py
@@ -1,21 +1,14 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
+connection = sqlite3.connect('database2.db')
 
-with open('schema.sql') as f:
+with open('schema2.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 
-# Ajouter des livres de testBBLA
-cur.execute("INSERT INTO Livres (Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?)", 
-            ('Emilie', 'Victor', 2024, 10))
-cur.execute("INSERT INTO Livres (Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?)", 
-            ('Didier', 'Laurent', 2023, 5))
-
-# Ajouter un utilisateur admin
-cur.execute("INSERT INTO Utilisateurs (Nom, Prenom, Email, Mot_de_passe, Role) VALUES (?, ?, ?, ?, ?)", 
-            ('Admin', 'Super', 'admin@example.com', 'admin123', 'admin'))
+cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(1, 'Emilie', 'Victor', 2024, 10))
+cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(2, 'Didier', 'Laurent', 2023, 5))
 
 connection.commit()
 connection.close()
